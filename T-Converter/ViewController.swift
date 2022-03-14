2627d83c6ab4d171b3dba6c5b9bb779a3c7c329f
Alex Asichka <sanechka3@mail.ru>
2022-03-14 13:10:34 +0200

    first commit

--- T-Converter/ViewController.swift
+++ T-Converter/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var celsiusLabel: UILabel!
+    
+    @IBOutlet weak var foringeitLabel: UILabel!
+    
+    @IBOutlet weak var slider: UISlider! {
+        didSet {
+            slider.value = 0
+            slider.minimumValue = 0
+            slider.maximumValue = 100
+            slider.tintColor = .yellow
+        }
     }
-
-
+    
+    
+    
+    
+    @IBAction func changeTemperature(_ sender: UISlider) {
+        let currentValue = Int(sender.value)
+        celsiusLabel.text = "\(currentValue)ºC"
+        foringeitLabel.text = "\(currentValue * 9 / 5 + 32)ºF"
+        
+    }
 }
 
